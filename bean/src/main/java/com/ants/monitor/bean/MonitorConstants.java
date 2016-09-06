eb841cb2fbb3a8fb225fa25674e56c39a59b134d
zxg <xigeng.zhong@tqmall.com>
2016-09-06 20:16:12 +0800

    注释

--- bean/src/main/java/com/ants/monitor/bean/MonitorConstants.java
+++ bean/src/main/java/com/ants/monitor/bean/MonitorConstants.java
@@ -23,15 +23,22 @@ public class MonitorConstants {
 
     /**=============ip 所在的服务器===================================**/
 
-    //存所有的ip：name
+    //存所有的----ip：name，例如 10.0.0.1：本地服务器
     public static final Map<String,String> ecsMap = new HashMap<>();
-    // 双向map
+    // 双向map---内网 ip:外网 ip
     public static final BiMap<String,String> ecsBiMap = HashBiMap.create();
-    //测试环境的ip
+    //测试环境的ip---内网 ip:外网 ip
     public static final Map<String,String> ecsTestMap = new HashMap<>();
 
     public static void initEcsMap(){
         //todo 自定义初始化上述map。将ip放置在内存中。
+//        ecsMap.put("10.0.0.1","测试服务器");
+//
+//        ecsBiMap.put("10.0.0.1","192.xxx.x0.xx");
+//        ecsBiMap.put("10.0.0.2","192.xxx.x1.xx");
+//
+//        ecsTestMap.put("10.0.0.2","192.xxx.x1.xx");
+
 
     }
 
